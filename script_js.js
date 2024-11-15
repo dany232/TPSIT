e475c432e931c603dfd5f318a32885180c8aa93d
Daniele Porcaro <130255250+dany232@users.noreply.github.com>
2024-11-15 12:23:23 +0100

    Add files via upload

---
+++ script_js.js
@@ -0,0 +1,42 @@
+// script.js
+const progetti = [
+    {
+        titolo: "JS 1",
+        descrizione: "JS 1",
+        immagine: "link-immagine-1.jpg",
+        link: "/JS/js1.html"
+    },
+    {
+        titolo: "JS 2",
+        descrizione: "JS 2",
+        immagine: "link-immagine-2.jpg",
+        link: "/JS/js2.html"
+    },
+    {
+        titolo: "JS 3",
+        descrizione: "JS 3",
+        immagine: "link-immagine-3.jpg",
+        link: "/JS/js3.html"
+    },
+    {
+        titolo: "JS 4",
+        descrizione: "JS 4",
+        immagine: "link-immagine-4.jpg",
+        link: "/JS/js4.html"
+    }
+];
+
+const sezioneProgetti = document.getElementById("progetti");
+
+progetti.forEach(progetto => {
+    const progettoDiv = document.createElement("div");
+    progettoDiv.classList.add("progetto");
+
+    progettoDiv.innerHTML = `
+        <h3>${progetto.titolo}</h3>
+        <p>${progetto.descrizione}</p>
+        <a href="${progetto.link}" target="_blank">Guarda il progetto</a>
+    `;
+
+    sezioneProgetti.appendChild(progettoDiv);
+});
