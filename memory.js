d8c27810eb71cfe553d39fdf2a6c1cddad802f8e
Daniele Porcaro <130255250+dany232@users.noreply.github.com>
2025-02-23 23:29:40 +0100

    Update memory.js

--- memory.js
+++ memory.js
@@ -2,7 +2,7 @@ const start = document.getElementById('start');
 cards = ['box-1', 'box-2', 'box-3', 'box-4', 'box-5', 'box-6', 'box-7', 'box-8', 'box-9']
 let tempSequence = [];
 let userSequence = [];
-let index = 4;
+let index = 2;
 let delay = 1500;
 function startMemory(){
     for(i = 0;i < index;i++){
